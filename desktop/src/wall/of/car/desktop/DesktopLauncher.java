1d72962245132676f174eadb78402f78db84fc7d
d4xee <daxeckertobias@gmail.com>
2021-10-25 12:35:29 +0200

    initial commit

---
+++ desktop/src/wall/of/car/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package wall.of.car.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import wall.of.car.TutorialGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialGame(), config);
+	}
+}
